a3a4396cd7538a8c4f1486aac119c90da82388ae
unknown <kashif_saleem_23@hotmail.com>
2022-11-16 19:59:10 +0500

    Add Modal Card and styling in Css

--- script.js
+++ script.js
@@ -10,128 +10,161 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
 }));
 
-// Projects Info //
+// const arr = ['hello', 'hi', 'bye'];
 
-
-const body = document.querySelector('body');
-const section = document.createElement('section');
-body.appendChild(section);
-section.innerHTML = `
-<h2>Project name goes here</h2>
-  <ul>
-    <li>Html/CSS</li>
-    <li>Ruby on Rails</li>
-    <li>JavaScript</li>
-  </ul>
-  <div class="swiper">
-    <!-- Additional required wrapper -->
-    <div class="swiper-wrapper">
-      <!-- Slides -->
-      <div class="swiper-slide"><img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver"></div>
-      <div class="swiper-slide"><img src="images/Rectangle 21 (1).svg" alt=""></div>
-      <div class="swiper-slide"><img src="images/Rectangle 21 (2).svg" alt=""></div>
-      <div class="swiper-slide"><img src="images/Rectangle 21 (3).svg" alt=""></div>
-    </div>
-    <!-- If we need pagination -->
-    <div class="swiper-pagination">
-      <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
-      <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
-      <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
-      <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
-    </div>
-
-    <!-- If we need navigation buttons -->
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-  </div>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quibusdam quas voluptatum adipisci accusamus culpa
-    cum voluptatibus dicta mollitia, commodi minima quaerat aliquam esse, repudiandae vitae nihil corrupti animi
-    expedita!</p><br>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quibusdam quas voluptatum adipisci accusamus culpa
-      cum voluptatibus dicta mollitia, commodi minima quaerat aliquam esse, repudiandae vitae nihil corrupti animi
-      expedita!</p>
-      <i class="fa-light fa-arrow-up-right-from-square"></i>
-  <button type="button">See live <img src="images/Union_2.svg" alt=""></button> <br>
-  <button type="button">See Source <img src="images/Vector.svg" alt=""></button>
-`;
-
-
-// Creating an array
-
-const projectInfo= [
-  {
-    projectTitle: 'Project #0',
-    description: 'project#0 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    mainImage: ''
-    images: [],
-    Technology: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-    linkToLiveVersion: '#'
-    linkToSource: 'https://github.com/Kashif-Saleem-Ghuman/',
-  }
-
+const modal = [
   {
     projectTitle: 'Project #1',
-    description: 'project#1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    mainImage: 
-    images: [],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: '',
+    images: 'src',
     Technology: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-    linkToLiveVersion: '#'
+    linkToLiveVersion: '#',
     linkToSource: 'https://github.com/Kashif-Saleem-Ghuman/',
-  }
+  },
 
   {
     projectTitle: 'Project #2',
-    description: 'project#2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    mainImage: 
-    images: [],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'src',
+    images: 'src',
     Technology: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-    linkToLiveVersion: '#'
+    linkToLiveVersion: '#',
     linkToSource: 'https://github.com/Kashif-Saleem-Ghuman/',
-  }
+  },
 
   {
     projectTitle: 'Project #3',
-    description: 'project#3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    mainImage: 
-    images: [],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'src',
+    images: 'src',
     Technology: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-    linkToLiveVersion: '#'
+    linkToLiveVersion: '#',
     linkToSource: 'https://github.com/Kashif-Saleem-Ghuman/',
-  }
+  },
 
   {
     projectTitle: 'Project #4',
-    description: 'project#4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    mainImage: 
-    images: [],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'src',
+    images: 'src',
     Technology: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-    linkToLiveVersion: '#'
+    linkToLiveVersion: '#',
     linkToSource: 'https://github.com/Kashif-Saleem-Ghuman/',
-  }
+  },
 
   {
     projectTitle: 'Project #5',
-    description: 'project#5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    mainImage: 
-    images: [],
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'src',
+    images: 'src',
     Technology: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
-    linkToLiveVersion: '#'
+    linkToLiveVersion: '#',
     linkToSource: 'https://github.com/Kashif-Saleem-Ghuman/',
-  }
+  },
+
+  {
+    projectTitle: 'Project #6',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    mainImage: 'src',
+    images: 'src',
+    Technology: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+    linkToLiveVersion: '#',
+    linkToSource: 'https://github.com/Kashif-Saleem-Ghuman/',
+  }];
+
   
-  ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 
+// Projects Info //
+// const body = document.querySelector('body');
+// const section = document.createElement('section');
+// body.appendChild(section);
+// section.innerHTML = `
+// <h2>Project name goes here</h2>
+//   <ul>
+//     <li>Html/CSS</li>
+//     <li>Ruby on Rails</li>
+//     <li>JavaScript</li>
+//   </ul>
+//   <div class="swiper">
+//     <!-- Additional required wrapper -->
+//     <div class="swiper-wrapper">
+//       <!-- Slides -->
+//       <div class="swiper-slide"><img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver"></div>
+//       <div class="swiper-slide"><img src="images/Rectangle 21 (1).svg" alt=""></div>
+//       <div class="swiper-slide"><img src="images/Rectangle 21 (2).svg" alt=""></div>
+//       <div class="swiper-slide"><img src="images/Rectangle 21 (3).svg" alt=""></div>
+//     </div>
+//     <!-- If we need pagination -->
+//     <div class="swiper-pagination">
+//       <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
+//       <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
+//       <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
+//       <img src="images/Rectangle 34.svg" alt="Laptop wit pink screensaver">
+//     </div>
+
+//     <!-- If we need navigation buttons -->
+//     <div class="swiper-button-prev"></div>
+//     <div class="swiper-button-next"></div>
+//   </div>
+//   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quibusdam quas voluptatum adipisci accusamus culpa
+//     cum voluptatibus dicta mollitia, commodi minima quaerat aliquam esse, repudiandae vitae nihil corrupti animi
+//     expedita!</p><br>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quibusdam quas voluptatum adipisci accusamus culpa
+//       cum voluptatibus dicta mollitia, commodi minima quaerat aliquam esse, repudiandae vitae nihil corrupti animi
+//       expedita!</p>
+//       <i class="fa-light fa-arrow-up-right-from-square"></i>
+//   <button type="button">See live <img src="images/Union_2.svg" alt=""></button> <br>
+//   <button type="button">See Source <img src="images/Vector.svg" alt=""></button>
+// `;
 
 
+// // Creating an array
+// const main_Project = document.querySelector ('desktop-container-grid')
+
+// function createProjectCard(arg)
+// {
+//   const projectCard = document.createElement ('div')
+//   projectCard.classList.add('projects')
+//   main_Project.appendChild(projectCard)
+// }
 
 
+// const open = document.querySelector(".see-more");
+// open.addEventListener('click',()=>{
+//  const mainProject = document.createElement('div')
+//  const popupCard = document.createElement ('div')
+ 
+//  popupCard.innerHTML = `
+//  for ( let i=0; i<projectInfo.length; i++)
+//  {
+//   createProjectCard(projectInfo[i])
+//  }
+//  `
 
-const open = document.querySelector(".see-more");
-open.addEventListener('click',()=>{
-  const projects = document.createElement('div');
-  const popup =  document.createElement('div');
-
-  popup.innerHTML
-}
-)
+// })
